b701da1e1dfcc8ee1e76268ce2b3282c532c6324
Pavel <mankov.174@gmail.com>
2025-02-01 23:20:40 +0500

    Update project

--- src/test/java/ru/netology/test/CardDeliveryTest.java
+++ src/test/java/ru/netology/test/CardDeliveryTest.java
@@ -19,16 +19,6 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveryTest {
 
-    @BeforeAll
-    static void setUpAll() {
-        SelenideLogger.addListener("allure", new AllureSelenide());
-    }
-
-    @AfterAll
-    static void tearDownAll() {
-        SelenideLogger.removeListener("allure");
-    }
-
     @BeforeEach
     void setup() {
         open("http:localhost:9999");
